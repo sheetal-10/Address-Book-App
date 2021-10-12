ffa6161e4ac07091e1080d26888a36f1cf514b44
sheetal-10 <sheetalp257@gmail.com>
2021-10-12 23:18:02 +0530

    Added: project files from spring initialiser

---
+++ src/test/java/com/bridgelabz/Address/Book/App/AddressBookAppApplicationTests.java
@@ -0,0 +1,13 @@
+package com.bridgelabz.Address.Book.App;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class AddressBookAppApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
